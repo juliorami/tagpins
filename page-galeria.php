845aaa05d117d37ce9b370dc12b3aa4df9131277
julio <j.ramirez@nuevaweb.com.mx>
2017-11-27 18:34:00 -0600

    added faq page and gallery

---
+++ page-galeria.php
@@ -0,0 +1,4 @@
+<?php /* Template Name: galeria */ ?>
+	<?php get_header() ?>
+
+<?php get_footer() ?>
---
+++ page-preguntas.php
@@ -0,0 +1,38 @@
+<?php /* Template Name: preguntas frecuentes */ ?>
+	<?php get_header() ?>
+
+<div class="list">
+	<div class="container">
+			<h2>PREGUNTAS FRECUENTES</h2>
+		<div class="row">
+			<div class="col-4">
+				<img src="<?php bloginfo('template_url'); ?>/img/envio.png">
+				<h1>envio</h1>
+				<p>Contamos con envio a toda la toda la República y algunos envios internacionales...</p>
+				<img src="<?php bloginfo('template_url'); ?>/img/tiempo.png">
+				<h1>tiempo de envio</h1>
+				<p>Aproximadamente el envio tarda de 15 a 20 días habiles en envios nacvionales, internacionales 30 días.</p>
+			</div>
+			<div class="col-4">
+				<img class="tama" src="<?php bloginfo('template_url'); ?>/img/tamaño.png">
+				<h1>tamaño</h1>
+				<p>La especificación de tamaños viene en la descripción de cada productos, el tamaño varia según el modelo.</p>
+			</div>
+			<div class="col-4">
+				<img src="<?php bloginfo('template_url'); ?>/img/garantia.png">
+				<h1>garantia</h1>
+				<p>Nuestros productos estan elaborados con materiales de alta gama, por lo cual este contiene una garantia de 2 años, por defectos de fabrica, o detalles en el uso.</p>
+			</div>
+		</div>
+	</div>
+
+</div>
+<?php get_footer() ?>
+
+
+
+
+cuantos tamaños hay
+hay envios a toda la republica
+cuanto tiempo tarda en llegar
+tiene garantia
