5a87031bfe8ad432041dfdc6ab91b34ec0066036
julio <j.ramirez@nuevaweb.com.mx>
2018-01-12 14:27:19 -0600

    update de version de css para evitar problema de caché en server

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_action( 'init', 'tp_register_menu' );
 
 function tp_tagpin_scripts() {
     wp_enqueue_style( 'tagpin_theme_css', get_stylesheet_uri() );
-    wp_enqueue_style( 'tagpin_css', get_template_directory_uri()."/css/main.css" );
+    wp_enqueue_style( 'tagpin_css', get_template_directory_uri()."/css/main.css", arra(), '5.0.0' );
     wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css' );
     wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js', array(), '1.0.0', true );
 }
